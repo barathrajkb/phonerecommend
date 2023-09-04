5491c8ccf4601636ab7abc0bbf1afc876979e7d9
Barath Raj K B <72440183+barathrajkb@users.noreply.github.com>
2023-09-04 18:17:57 +0530

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import mysql.connector
-from db import connect_to_db
+from db import connect_online
+import streamlit as st
+import psycopg2
 def insert_data_from_csv(connection, csv_file_path):
-    # Read data from CSV file
     df = pd.read_csv(csv_file_path)
 
-    # Insert data into the table
     columns = ', '.join(df.columns)
     placeholders = ', '.join(['%s'] * len(df.columns))
     query = f'INSERT INTO abcheck ({columns}) VALUES ({placeholders})'
@@ -18,18 +17,6 @@ def insert_data_from_csv(connection, csv_file_path):
     connection.commit()
     cursor.close()
 
-if __name__ == '__main__':
-
-    conn = connect_to_db()
-
-    csv_file_path = 'dat.csv'
-
-    insert_data_from_csv(conn, csv_file_path)
-
-    conn.close()
-
-    print(f"Data has been inserted successfully into the table.")
-
 def insert_values(connection, data):
     try:
         cursor = connection.cursor()
@@ -37,6 +24,28 @@ def insert_values(connection, data):
         cursor.execute(insert_query, data)
         connection.commit()
         st.success("Data inserted successfully!")
-    except mysql.connector.Error as err:
+    except psycopg2.Error as err:
         st.error(f"Error: {err}")
-        connection.rollback()
+        if connection:
+            connection.rollback()
+
+def create_db():
+    conn = connect_online()
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS abcheck (id int primary key, brand VARCHAR(30), model VARCHAR(50), operating_system VARCHAR(10), internal_memory INT, RAM INT, performance INT, main_camera INT, selfie_camera INT, battery_size INT, screen_size FLOAT, weight FLOAT, price FLOAT)")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def insert_online_from_csv(connection, csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    n = len(df)
+    columns = ', '.join(df.columns)
+    placeholders = ', '.join(['%s'] * len(df.columns))
+    cursor = connection.cursor()
+    query = f'INSERT INTO abcheck ({columns}) VALUES ({placeholders})'
+    for row in df.itertuples(index=False):
+        row_values = [None if pd.isna(value) else value for value in row]
+        cursor.execute(query, row_values)
+    connection.commit()
+    cursor.close()
